f421405f32ca2db479696d1a434e0d3a0cc94b0b
martinsk <msk@tigertext.com>
2013-11-07 16:06:38 -0800

    clean ups

--- apps/eTally/src/etally.erl
+++ apps/eTally/src/etally.erl
@@ -1,13 +1,38 @@
 -module(etally).
--export([send_event/1, get_counter/1]).
+-export([send_event/3, get_counter/2,
+         get_leaderboard/3, list_leaderboards/1]).
 
-send_event(Ls) ->
-    cast_cnode({event, Ls, []}).
-get_counter(Name) ->
-    call_cnode({get_counter, Name}).
+-spec send_event(Ls, LBBindings, Node) -> ok when
+      Ls ::  [binary()],
+      LBBindings :: [{binary(), binary()}],
+      Node :: atom().
 
-call_cnode(Msg) ->
-    {any, 'cnode@msk-dev.local'} ! {call, self(), Msg},
+send_event(Ls, LBBindings, Node) ->
+    cast_cnode({event, Ls, LBBindings}, Node).
+
+
+-spec get_counter(Name, Node) -> ok when
+      Name :: binary(),
+      Node :: atom().
+
+get_counter(Name, Node) ->
+    call_cnode({get_counter, Name}, Node).
+
+
+get_leaderboard(Name, Interval, Node) ->
+    call_cnode({get_leaderboard, Name, Interval}, Node).
+
+list_leaderboards(Node) ->
+    call_cnode({list_leaderboards}, Node).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% actual methods for sending the data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+call_cnode(Msg, Node) ->
+    {any, Node} ! {call, self(), Msg},
     receive
 	Result ->
 	    Result
@@ -16,7 +41,7 @@ call_cnode(Msg) ->
             ok
     end.
 
-cast_cnode(Msg) ->
-    {any, 'cnode@msk-dev.local'} ! {call, self(), Msg},
+cast_cnode(Msg, Node) ->
+    {any, Node} ! {call, self(), Msg},
     ok.
     
