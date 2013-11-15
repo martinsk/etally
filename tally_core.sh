75e7e2817d7594bd6bba4424b9fd5e9d9bd73cc6
martinsk <msk@tigertext.com>
2013-11-15 10:20:21 -0800

    updated to nice filename

--- tally_core.sh
+++ tally_core.sh
@@ -4,5 +4,6 @@ git clone https://github.com/martinsk/etally_core.git
 cd etally_core
 make
 cd .. 
-cp etally_core/etally_srv priv/
+mkdir priv
+cp etally_core/etally_srv priv/etally_srv
 rm -rf etally_core
