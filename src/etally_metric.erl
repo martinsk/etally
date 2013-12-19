e706e57b52900081131a8eece3a4eb17a194e6d6
martinsk <msk@tigertext.com>
2013-12-18 17:03:09 -0800

    typo

--- src/etally_metric.erl
+++ src/etally_metric.erl
@@ -3,25 +3,27 @@
          get_leaderboard/4, list_leaderboards/1,
          send_event_bootstrap/5]).
 
--spec send_event(Ls, LBBindings, Payload, Node) -> ok when
+-spec send_event(Ls, LBBindings, Percents, Payload, Node) -> ok when
       Ls         :: [binary()],
       LBBindings :: [{binary(), binary()}],
+      Percents   :: [binary()],
       Payload    :: pos_integer(),
       Node       :: atom().
 
-send_event(Ls, LBBindings, Payload, Node) ->
-    etally_utils:cast_cnode({metric_event, Ls, LBBindings, Payload}, Node).
+send_event(Ls, LBBindings, Percents, Payload, Node) ->
+    etally_utils:cast_cnode({metric_event, Ls, LBBindings, Percents, Payload}, Node).
 
 
--spec send_event_bootstrap(Ls, LBBindings, Ts, Payload, Node) -> ok when
+-spec send_event_bootstrap(Ls, LBBindings, Percents, Ts, Payload, Node) -> ok when
       Ls         :: [binary()],
       LBBindings :: [{binary(), binary()}],
+      Percents   :: [binary()],
       Ts         :: pos_integer(),
       Payload    :: pos_integer(),
       Node       :: atom().
 
-send_event_bootstrap(Ls, LBBindings, Ts, Payload, Node) ->
-    etally_utils:cast_cnode({metric_event_tz, Ls, LBBindings, Ts, Payload}, Node).
+send_event_bootstrap(Ls, LBBindings, Percents, Ts, Payload, Node) ->
+    etally_utils:cast_cnode({metric_event_tz, Ls, LBBindings, Percents, Ts, Payload}, Node).
 
 
 -spec get_counter(Name, Node) -> ok when
